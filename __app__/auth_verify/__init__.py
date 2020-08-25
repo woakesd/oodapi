16e7c60655aab87b036a00054975b0d8e664c5f3
David Woakes <david@woak.es>
2020-08-25 11:38:55 +0100

    Change verify endpoint to use Bearer token and return payload if valid

--- __app__/auth_verify/__init__.py
+++ __app__/auth_verify/__init__.py
@@ -6,31 +6,14 @@ from base64 import b64encode, b64decode
 from json import dumps
 from jwt import JWT, jwk_from_pem
 from jwt.exceptions import JWTDecodeError
+from __app__.auth_lib import authorization
 
-def get_signing_key():
-    private_key_data = os.environ['privateRSAKey']
-    return jwk_from_pem(b64decode(os.environ['privateRSAKey']))
-
-
-def verify_jwt(token):
-
-    signing_key = get_signing_key()
-
-    instance = JWT()
-
-    return instance.decode(token, signing_key)
-
-async def main(req: azure.functions.HttpRequest) -> azure.functions.HttpResponse:
+@authorization
+async def main(req: azure.functions.HttpRequest, payload) -> azure.functions.HttpResponse:
     try:
-        token = req.route_params.get('token')
-        logging.debug(f'Got token {token}')
+        logging.debug(f'Got payload {payload}')
 
-        try:
-            message = verify_jwt(token)
-        except JWTDecodeError as e:
-            return azure.functions.HttpResponse(dumps({'error': e}, default=str), status_code=401)
-
-        return azure.functions.HttpResponse(dumps(message, default=str), status_code=200)
+        return azure.functions.HttpResponse(dumps(payload, default=str), status_code=200)
 
     except Exception as e:
         logging.error(f"Failure, {e}")
